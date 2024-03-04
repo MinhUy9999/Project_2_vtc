757dc8032766e9630aa9870a42a077685e9c079b
MinhUy999 <tpmudat1999@gmai.com>
2024-03-04 12:54:09 +0700

    3/4

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <!-- FontAwesome 6.2.0 CSS -->
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <link rel="stylesheet" href="./css/style.css?v= <?php echo time(); ?>">
+     
+        
+    </head>
+
+    <body class="sticky-tr sticky-header transparent-header">
+        <header class="header-v2" style="top:0px">
+           <div class="top-bar">
+              <div class="alignwide">
+                  <div class="left">
+                    <ul class="info">
+                        <li>
+                        <i class="fa-solid fa-location-dot"></i>
+                            123 Main Street, uni 21, new york, city
+                        </li>
+                        <li>
+                            <a href="#">
+                            <i class="fa-solid fa-phone"></i>
+                                +860948134463
+                            </a>
+                        </li>
+                    </ul>
+                  </div>
+                  <div class="right">
+                    <ul class="additional-links">
+                        <li>
+                            <a href="#">contact</a>
+                        </li>
+                    </ul>
+                    <div class="social">
+                        <a  href="" target="blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-tiktok"></i></a>
+                    </div>
+                  </div>
+              </div>
+           </div>
+           <div class="inner alignwide">
+               
+           </div>
+        </header>
+        <main>
+            <h1>minh uy</h1>
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
