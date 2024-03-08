97f68be244f52f39dad7c979a3ee2b9819d8a289
MinhUy999 <tpmudat1999@gmai.com>
2024-03-08 15:33:58 +0700

    8/3

--- index.php
+++ index.php
@@ -64,8 +64,8 @@
            <div class="inner alignwide">
                <div id="nav-listener" class="nav-icon-left">
                 <span></span>
-                <span class="top:8px"></span>
-                <span class="top:16px"></span>
+                <span ></span>
+                <span ></span>
                </div>
                <div class="logo">
                    <div class="text-logo">
@@ -75,12 +75,148 @@
                     </a>
                    </div>
                </div>
+        <nav class="site-nav">
+			<ul id="menu-navigation" class="primary-menu"><li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-41 current_page_item menu-item-42"><a href="#" aria-current="page">Home</a></li>
+        <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-19"><a href="#">Our Menu<i class="fa-solid fa-caret-down"></i></a>
+        <button class="dropdown-toggle"></button>
+           <ul class="sub-menu">
+	<li id="menu-item-1218" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1218"><a href="#">Boxed Layout</a></li>
+           </ul>
+          </li>
+          <li id="menu-item-384" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-384"><a href="#">Shop<i class="fa-solid fa-caret-down"></i></a><button class="dropdown-toggle"></button>
+         <ul class="sub-menu">
+	<li id="menu-item-1211" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1211"><a href="#">Pizza</a></li>
+	<li id="menu-item-1212" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1212"><a href="#">Salads</a></li>
+	<li id="menu-item-1213" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1213"><a href="$">Soups</a></li>
+	<li id="menu-item-1214" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1214"><a href="#">Desserts</a></li>
+	<li id="menu-item-1215" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1215"><a href="#">Drinks</a></li>
+        </ul>
+           </li>
+            <li id="menu-item-117" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-117"><a href="#">Gallery</a></li>
+           <li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45"><a href="#">Our News</a></li>
+             <li id="menu-item-39" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-39"><a href="#">Reservation<i class="fa-solid fa-caret-down"></i></a><button class="dropdown-toggle"></button>
+             <ul class="sub-menu">
+	     <li id="menu-item-286" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-286"><a href="#">Open Table Reservation</a></li>
+       </ul>
+         </li>
+        <li id="menu-item-36" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"><a href="#">Contact Us</a></li>
+       </ul>	
+     <div class="shopping-cart">
+			<div class="cart-listener">
+				<span class="cart-link">
+                <i class="fa-solid fa-basket-shopping"></i>
+	<span class="count">0</span>
+	</span>			
+       </div>
+			<div class="header-mini-cart">
+				<p class="cart-links">
+					<a href="#"><i class="fa-solid fa-user"></i>Sign In</a>
+					<a href="#"><i  class="fa-solid fa-basket-shopping" aria-hidden="true"></i>View cart</a>
+					<a class="close-cart icon-cancel"></a>
+				</p>
+				<div class="widget woocommerce widget_shopping_cart"><h2 class="widgettitle">Cart</h2>
+                <div class="widget_shopping_cart_content">
+                <p class="woocommerce-mini-cart__empty-message">No products in the cart.</p>
+		   </div>
+        </div>
+			</div>	
+      </nav>	
+		</div>		
+		</header>
+       <section id="slider">
+       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="./images/banner1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="./images/banner2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/banner3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+ <div class="info">
+    <h1>Cristiano Restaurant</h1>
+    <div class="title">
+        <p>123 Main Street, Uni 21, New York City</p>
+    </div>
+ </div>
+       </section>
+       <section class="order">
+              <div class="container">
+                    <div class="content_order">
+                           <div class="order_title">
+                             <p>From 11:00am to 10:00pm</p>
+                             <h3> Order Online</h3>
+                           </div>
+                    </div>
+                    <div class="item_order">
+    <div class="container">
+        <div class="image">
+            <li class="product">
+                <a href="#"><img src="./images/Salads.jpg" alt=""></a> 
+            </li>
+        </div>
+        <div class="image">
+            <li class="product">
+                <a href="#"><img src="./images/Salads.jpg" alt=""></a> 
+            </li>
+        </div>
+        <div class="image">
+            <li class="product">
+                <a href="#"><img src="./images/Salads.jpg" alt=""></a> 
+            </li>
+        </div>
+        <div class="image">
+            <li class="product">
+                <a href="#"><img src="./images/Salads.jpg" alt=""></a> 
+            </li>
+        </div>
+    </div>
+</div>
+
+              </div>
+       </section>
+      <footer id="footer" class="footer">
+           <div class="footer-widgets">
+                <div id="cristino_contact-3" class="widget widget_cristiano_contact">
+                    <h2 class="font-heading">Contact Us</h2>
+                    <ul class="contact-info">
+                        <li>123 Main Street, Uni 21, New York City</li>
+                        <li><a href="#">+38 (012) 34 56 789</a></li>
+                        <li class="hours">Mon - Fri: 08:00 - 22:00</li>
+                        <li class="hours">Mon - Fri: 08:00 - 22:00</li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                    </ul>
+                </div>
+                <div id="cristiano_social-3" class="widget widget_cristiano_social">
+                    <h2 class="font-heading"></h2>
+                    <p>Join us on social networks</p>
+                    <div class="social">
+                        <a  href="" target="blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-tiktok"></i></a>
+                    </div>
+                </div>
            </div>
-        </header>
-       
-        <footer>
-            <!-- place footer here -->
-        </footer>
+           <div class="bottom-bar">
+            <p>Copyright © 2022 Cristiano. All rights reserved.</p>
+           </div>
+      </footer>
+      <div class="overlay"></div>
         <!-- Bootstrap JavaScript Libraries -->
         <script
             src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
