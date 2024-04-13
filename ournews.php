b634f27b6188265c700d9b0beacb57068ef4aa9c
Vi Khang <khangvokhang87@gmail.com>
2024-04-13 14:19:49 +0700

    Them code

---
+++ ournews.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <!-- FontAwesome 6.2.0 CSS -->
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="./css/style.css?v= <?php echo time(); ?>">
+    </head>
+
+    
+    <body class="sticky-tr sticky-header transparent-header">
+        <header class="header-v2" style="top:0px">
+           <div class="top-bar">
+              <div class="alignwide">
+                  <div class="left">
+                    <ul class="info">
+                        <li>
+                        <i class="fa-solid fa-location-dot"></i>
+                            123 Main Street, uni 21, new york, city
+                        </li>
+                        <li>
+                            <a href="#">
+                            <i class="fa-solid fa-phone"></i>
+                                +860948134463
+                            </a>
+                        </li>
+                    </ul>
+                  </div>
+                  <div class="right">
+                    <ul class="additional-links">
+                        <li>
+                            <a href="#">contact</a>
+                        </li>
+                    </ul>
+                    <div class="social">
+                        <a  href="" target="blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-tiktok"></i></a>
+                    </div>
+                  </div>
+              </div>
+           </div>
+           <div class="inner alignwide">
+               <div id="nav-listener" class="nav-icon-left">
+                <span></span>
+                <span ></span>
+                <span ></span>
+               </div>
+               <div class="logo">
+                   <div class="text-logo">
+                    <a href="index.php">
+                        <span class="title">Cristiano</span>
+                        <span class="description">Restaurant</span>
+                    </a>
+                   </div>
+               </div>
+        <nav class="site-nav">
+			<ul id="menu-navigation" class="primary-menu"><li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-41 current_page_item menu-item-42"><a href="#" aria-current="page">Home</a></li>
+        <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-19"><a href="#">Our Menu<i class="fa-solid fa-caret-down"></i></a>
+        <button class="dropdown-toggle"></button>
+           <ul class="sub-menu">
+	<li id="menu-item-1218" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1218"><a href="#">Boxed Layout</a></li>
+           </ul>
+          </li>
+          <li id="menu-item-384" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-384"><a href="#">Shop<i class="fa-solid fa-caret-down"></i></a><button class="dropdown-toggle"></button>
+         <ul class="sub-menu">
+	<li id="menu-item-1211" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1211"><a href="#">Pizza</a></li>
+	<li id="menu-item-1212" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1212"><a href="#">Salads</a></li>
+	<li id="menu-item-1213" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1213"><a href="$">Soups</a></li>
+	<li id="menu-item-1214" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1214"><a href="#">Desserts</a></li>
+	<li id="menu-item-1215" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-1215"><a href="#">Drinks</a></li>
+        </ul>
+           </li>
+            <li id="menu-item-117" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-117"><a href="#">Gallery</a></li>
+           <li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45"><a href="#">Our News</a></li>
+             <li id="menu-item-39" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-39"><a href="#">Reservation<i class="fa-solid fa-caret-down"></i></a><button class="dropdown-toggle"></button>
+             <ul class="sub-menu">
+	     <li id="menu-item-286" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-286"><a href="#">Open Table Reservation</a></li>
+       </ul>
+         </li>
+        <li id="menu-item-36" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"><a href="#">Contact Us</a></li>
+       </ul>	
+     <div class="shopping-cart">
+			<div class="cart-listener">
+				<span class="cart-link">
+                <i class="fa-solid fa-basket-shopping"></i>
+	<span class="count">0</span>
+	</span>			
+       </div>
+			<div class="header-mini-cart">
+				<p class="cart-links">
+					<a href="#"><i class="fa-solid fa-user"></i>Sign In</a>
+					<a href="#"><i  class="fa-solid fa-basket-shopping" aria-hidden="true"></i>View cart</a>
+					<a class="close-cart icon-cancel"></a>
+				</p>
+				<div class="widget woocommerce widget_shopping_cart"><h2 class="widgettitle">Cart</h2>
+                <div class="widget_shopping_cart_content">
+                <p class="woocommerce-mini-cart__empty-message">No products in the cart.</p>
+		   </div>
+        </div>
+			</div>	
+      </nav>	
+		</div>	
+    </header>
+    <div id="page-header" class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll animation-engine-js loaded dzsprx-readyall under-760">
+    <div class="divimage dzsparallaxer--target lazyloaded" data-bg="https://cristianonew.ukrdevs.com/wp-content/uploads/2018/05/header-bg.jpg" style="background-image: url(&quot;https://cristianonew.ukrdevs.com/wp-content/uploads/2018/05/header-bg.jpg&quot;); transform: translate3d(0px, -116.277px, 0px);"></div>
+								<div class="heading-title">
+						<div class="center">
+							<h1 class="font-heading color-tr dynamic-shadow">OUR  NEWS</h1>
+													</div>
+					</div>
+				<span class="shading gradient"></span>
+</div>
+<div class="wrap">
+    <div id="container">
+        <main id="content">
+        <article id="post-325" class="article post-325 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+		<div class="image">
+		<a href="https://cristianonew.ukrdevs.com/where-can-i-get-some/">
+			<img width="675" height="439" src="/images/news1.jpg" class="attachment-medium size-medium wp-post-image" alt="" decoding="async" fetchpriority="high" srcset="https://cristianonew.ukrdevs.com/wp-content/uploads/2018/04/news-1-675x439.jpg 675w, https://cristianonew.ukrdevs.com/wp-content/uploads/2018/04/news-1-370x241.jpg 370w, https://cristianonew.ukrdevs.com/wp-content/uploads/2018/04/news-1-768x499.jpg 768w, https://cristianonew.ukrdevs.com/wp-content/uploads/2018/04/news-1-830x540.jpg 830w, https://cristianonew.ukrdevs.com/wp-content/uploads/2018/04/news-1.jpg 1920w" sizes="(max-width: 675px) 100vw, 675px">		</a>
+	</div>	
+		<div class="details">	
+		<time class="date" datetime="2018-04-03">April 3, 2018</time>	
+		<h2><a href="https://cristianonew.ukrdevs.com/where-can-i-get-some/">Where can I get some?</a></h2>
+		<div class="content">
+			<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly…</p>
+		</div>
+		<a class="btn-plate" href="https://cristianonew.ukrdevs.com/where-can-i-get-some/">Read More</a>
+	</div>
+</article>
+<article id="post-166" class="article post-166 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-ipsum tag-lorem tag-text">
+		<div class="image">
+		<a href="https://cristianonew.ukrdevs.com/where-does-it-come-from/">
+			<img width="675" height="450" src="/images/news2.jpg" class="attachment-medium size-medium wp-post-image" alt="" decoding="async" srcset="https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant-675x450.jpg 675w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant-370x247.jpg 370w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant-768x512.jpg 768w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant-830x553.jpg 830w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant-585x390.jpg 585w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/restaurant.jpg 1920w" sizes="(max-width: 675px) 100vw, 675px">		</a>
+	</div>	
+		<div class="details">	
+		<time class="date" datetime="2018-03-06">March 6, 2018</time>	
+		<h2><a href="https://cristianonew.ukrdevs.com/where-does-it-come-from/">Where does it come from?</a></h2>
+		<div class="content">
+			<p>Pellentesque nulla justo, auctor ac maximus sed,&nbsp;tempus sed nibh. Ut elit sapien, ornare et diam ac, efficitur luctus elit. Proin ut nulla consequat, elementum lacus eu, cursus ipsum. Fusce efficitur…</p>
+		</div>
+		<a class="btn-plate" href="https://cristianonew.ukrdevs.com/where-does-it-come-from/">Read More</a>
+	</div>
+</article>
+<article id="post-164" class="article post-164 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized tag-ipsum tag-lorem tag-text">
+		<div class="image">
+		<a href="https://cristianonew.ukrdevs.com/what-is-lorem-ipsum/">
+			<img width="675" height="450" src="https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-675x450.jpg" class="attachment-medium size-medium wp-post-image" alt="" decoding="async" srcset="https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-675x450.jpg 675w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-370x247.jpg 370w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-768x512.jpg 768w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-830x553.jpg 830w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2-585x390.jpg 585w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-2.jpg 1920w" sizes="(max-width: 675px) 100vw, 675px">		</a>
+	</div>	
+		<div class="details">	
+		<time class="date" datetime="2016-08-05">August 5, 2016</time>	
+		<h2><a href="https://cristianonew.ukrdevs.com/what-is-lorem-ipsum/">What is Lorem Ipsum?</a></h2>
+		<div class="content">
+			<p>Pellentesque nulla justo, auctor ac maximus sed,&nbsp;tempus sed nibh. Ut elit sapien, ornare et diam ac, efficitur luctus elit. Proin ut nulla consequat, elementum lacus eu, cursus ipsum. Fusce efficitur…</p>
+		</div>
+		<a class="btn-plate" href="https://cristianonew.ukrdevs.com/what-is-lorem-ipsum/">Read More</a>
+	</div>
+</article>
+<article id="post-77" class="article post-77 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+		<div class="image">
+		<a href="https://cristianonew.ukrdevs.com/summer-begin/">
+			<img width="675" height="450" src="https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-675x450.jpg" class="attachment-medium size-medium wp-post-image" alt="" decoding="async" loading="lazy" srcset="https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-675x450.jpg 675w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-370x247.jpg 370w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-768x512.jpg 768w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-830x553.jpg 830w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1-585x390.jpg 585w, https://cristianonew.ukrdevs.com/wp-content/uploads/2016/08/bar-1.jpg 1920w" sizes="(max-width: 675px) 100vw, 675px">		</a>
+	</div>	
+		<div class="details">	
+		<time class="date" datetime="2016-07-28">July 28, 2016</time>	
+		<h2><a href="https://cristianonew.ukrdevs.com/summer-begin/">Summer Begin!</a></h2>
+		<div class="content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi…</p>
+		</div>
+		<a class="btn-plate" href="https://cristianonew.ukrdevs.com/summer-begin/">Read More</a>
+	</div>
+</article>
+        </main>
+    </div>
+</div>
+
+<!--Footer!-->
+<footer id="footer" class="footer">
+           <div class="footer-widgets">
+                <div id="cristino_contact-3" class="widget widget_cristiano_contact">
+                    <h2 class="font-heading">Contact Us</h2>
+                    <ul class="contact-info">
+                        <li>123 Main Street, Uni 21, New York City</li>
+                        <li><a href="#">+38 (012) 34 56 789</a></li>
+                        <li class="hours">Mon - Fri: 08:00 - 22:00</li>
+                        <li class="hours">Mon - Fri: 08:00 - 22:00</li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                        <li class="hours"></li>
+                    </ul>
+                </div>
+                <div id="cristiano_social-3" class="widget widget_cristiano_social">
+                    <h2 class="font-heading"></h2>
+                    <p>Join us on social networks</p>
+                    <div class="social">
+                        <a  href="" target="blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="" target="blank"><i class="fa-brands fa-tiktok"></i></a>
+                    </div>
+                </div>
+           </div>
+           <div class="bottom-bar">
+            <p>Copyright © 2022 Cristiano. All rights reserved.</p>
+           </div>
+      </footer>
+      <div class="overlay"></div>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
